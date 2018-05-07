e862069641b8d0fdf1057f8c512d406f506d5352
Fırat Sivrikaya <sivrikayafirat@gmail.com>
2018-05-07 05:02:06 +0300

    Circle page fix

--- dev/app/src/main/java/world/augma/ui/circle/UICirclePage.java
+++ dev/app/src/main/java/world/augma/ui/circle/UICirclePage.java
@@ -2,20 +2,35 @@ package world.augma.ui.circle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import world.augma.R;
 
 public class UICirclePage extends AppCompatActivity {
+
+    //userID, username, circleID, circleNAME
+    private String circleID;
+    private String circleName;
+    private String circleDescription;
+    private TextView circlePageCircleName;
+    private TextView circleDescriptionField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_circle_page);
 
+        circleID = getIntent().getExtras().getString("circleID");
+        circleName = getIntent().getExtras().getString("name");
+        circleDescription = getIntent().getExtras().getString("desc");
 
+        circlePageCircleName = (TextView) findViewById(R.id.circlePageCircleName);
+        circleDescriptionField = (TextView) findViewById(R.id.circleDescription);
 
+        circleDescriptionField.setText(circleDescription);
+        circlePageCircleName.setText(circleName);
 
-
+        //TODO aws
 
     }
 
--- dev/app/src/main/java/world/augma/ui/widget/CircleCanvas.java
+++ dev/app/src/main/java/world/augma/ui/widget/CircleCanvas.java
@@ -1,7 +1,9 @@
 package world.augma.ui.widget;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,15 +14,20 @@ import android.widget.GridLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+
 import java.util.List;
 
 import world.augma.R;
 import world.augma.asset.Circle;
+import world.augma.ui.circle.UICircle;
+import world.augma.ui.circle.UICirclePage;
 
 public class CircleCanvas extends GridLayout {
 
     private Animation circleCreation;
     private Animation circleGrow;
+    private Circle clickedCircle;
 
     public CircleCanvas(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -35,11 +42,15 @@ public class CircleCanvas extends GridLayout {
         circleCreation = AnimationUtils.loadAnimation(getContext(), R.anim.circle_creation);
         circleGrow = AnimationUtils.loadAnimation(getContext(), R.anim.circle_page_grow);
 
+        circleGrow.setAnimationListener(new CircleAnimationListener());
+
         if(circleList != null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             CircleSelectionListener listener = new CircleSelectionListener();
 
             for(Circle circle : circleList) {
+                clickedCircle = circle;
+
                 RelativeLayout obj = (RelativeLayout) inflater.inflate(R.layout.circle_representation, null, false);
                 ((TextView) obj.findViewById(R.id.circleCenter)).setText(circle.getName());
                 RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams((int) circle.getRadius(), (int) circle.getRadius());
@@ -61,6 +72,48 @@ public class CircleCanvas extends GridLayout {
             obj.setLayoutParams(v.getLayoutParams());
             ((ViewGroup) getParent()).addView(obj);
             obj.startAnimation(circleGrow);
+        }
+    }
+
+    private class CircleAnimationListener implements Animation.AnimationListener {
+
+        /**
+         * <p>Notifies the start of the animation.</p>
+         *
+         * @param animation The started animation.
+         */
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        /**
+         * <p>Notifies the end of the animation. This callback is not invoked
+         * for animations with repeat count set to INFINITE.</p>
+         *
+         * @param animation The animation which reached its end.
+         */
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+            Intent intent = new Intent(getContext(), UICirclePage.class);
+            intent.putExtra("name", clickedCircle.getName());
+            intent.putExtra("circleID", clickedCircle.getCircleID());
+            intent.putExtra("desc", clickedCircle.getDescription());
+
+            getContext().startActivity(intent,
+                    ActivityOptionsCompat.makeCustomAnimation(getContext(), R.anim.fade_in, R.anim.fade_out).toBundle());
+
+        }
+
+        /**
+         * <p>Notifies the repetition of the animation.</p>
+         *
+         * @param animation The animation which was repeated.
+         */
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
         }
     }
 }
